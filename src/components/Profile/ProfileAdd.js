881b5b939ae10a5890679e1787c125b207348f01
inwoo choi <65886958+Choiinwoo401@users.noreply.github.com>
2023-06-16 02:05:01 +0900

    err

--- src/components/Profile/ProfileAdd.js
+++ src/components/Profile/ProfileAdd.js
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
 import { Button } from 'primereact/button';
-import Avatar from 'react-avatar-edit';
 import axios from 'axios';
 
-const ProfileAdd = ({ onProfileSave, onClose, user_id }) => {
-  const [petName, setPetName] = useState('');
-  const [petSex, setPetSex] = useState('');
-  const [petBirth, setPetBirth] = useState('');
-  const [imgCrop, setImgCrop] = useState(null);
+const ProfileAdd = ({ onProfileSave, onClose, user_id, animal_name, sex, birth }) => {
+  const [petName, setPetName] = useState(animal_name || ''); // Initialize with an empty string
+  const [petSex, setPetSex] = useState(sex || ''); // Initialize with an empty string
+  const [petBirth, setPetBirth] = useState(birth || ''); // Initialize with an empty string
+  const [selectedImage, setSelectedImage] = useState(null);
 
   const handleUpdateButtonClick = () => {
     const profileData = {
@@ -15,17 +14,26 @@ const ProfileAdd = ({ onProfileSave, onClose, user_id }) => {
       animal_name: petName,
       sex: petSex,
       birth: petBirth,
-      images: imgCrop,
     };
-
+  
+    // Create a new FormData instance
+    const formData = new FormData();
+    formData.append('user_id', user_id);
+    formData.append('animal_name', petName);
+    formData.append('sex', petSex);
+    formData.append('birth', petBirth);
+    formData.append('imgCrop', selectedImage);
+  
+    // Send the profile data and selected image to the server
     axios
-      .post(`http://3.88.1.192:3000/api/diary/animal?user_id=${user_id}`, profileData)
+      .post('http://3.88.1.192:3000/api/diary/animal', formData)
       .then((response) => {
         console.log(response.data);
         onProfileSave(profileData);
         onClose();
       })
       .catch((error) => {
+        // Handle errors
         console.error(error);
       });
   };
@@ -34,22 +42,13 @@ const ProfileAdd = ({ onProfileSave, onClose, user_id }) => {
     setPetName('');
     setPetSex('');
     setPetBirth('');
-    setImgCrop(null);
+    setSelectedImage(null);
     onClose();
   };
 
-  const onCrop = (view) => {
-    setImgCrop(view);
-  };
-
-  const canvasProps = {
-    canvas: {
-      width: 200,
-      height: 200,
-      style: {
-        willReadFrequently: true,
-      },
-    },
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedImage(file);
   };
 
   return (
@@ -66,19 +65,15 @@ const ProfileAdd = ({ onProfileSave, onClose, user_id }) => {
         <label>입양일:</label>
         <input type="text" value={petBirth} onChange={(e) => setPetBirth(e.target.value)} />
       </div>
-      <div className="avatar-container">
-        <Avatar
-          {...canvasProps}
-          width={200}
-          height={200}
-          onCrop={onCrop}
-          onClose={() => setImgCrop(null)}
-        />
+      <div className="form-group">
+        <label>사진:</label>
+        <input type="file" accept="image/*" onChange={handleImageChange} />
       </div>
       <Button onClick={handleUpdateButtonClick} label="추가" />
       <Button onClick={onCloseModal} label="닫기" className="p-button-secondary" />
     </div>
   );
 };
+
 
 export default ProfileAdd;
--- src/components/Profile/ProfileChange.js
+++ src/components/Profile/ProfileChange.js
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
 import { Button } from 'primereact/button';
-import Avatar from 'react-avatar-edit';
 import axios from 'axios';
 
 const ProfileChange = ({ onProfileSave, onClose, user_id, animal_name, sex, birth }) => {
   const [petName, setPetName] = useState(animal_name || ''); // Initialize with an empty string
   const [petSex, setPetSex] = useState(sex || ''); // Initialize with an empty string
   const [petBirth, setPetBirth] = useState(birth || ''); // Initialize with an empty string
-  const [imgCrop, setImgCrop] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   const handleUpdateButtonClick = () => {
     const profileData = {
@@ -15,12 +14,19 @@ const ProfileChange = ({ onProfileSave, onClose, user_id, animal_name, sex, birt
       animal_name: petName,
       sex: petSex,
       birth: petBirth,
-      imgCrop,
     };
-    
-    // Send the profile data to the server
+  
+    // Create a new FormData instance
+    const formData = new FormData();
+    formData.append('user_id', user_id);
+    formData.append('animal_name', petName);
+    formData.append('sex', petSex);
+    formData.append('birth', petBirth);
+    formData.append('imgCrop', selectedImage);
+  
+    // Send the profile data and selected image to the server
     axios
-      .post(`http://3.88.1.192:3000/api/diary/animal?user_id=${user_id}`, profileData)
+      .post('http://3.88.1.192:3000/api/diary/animal', formData)
       .then((response) => {
         console.log(response.data);
         onProfileSave(profileData);
@@ -36,23 +42,13 @@ const ProfileChange = ({ onProfileSave, onClose, user_id, animal_name, sex, birt
     setPetName('');
     setPetSex('');
     setPetBirth('');
-    setImgCrop(null);
+    setSelectedImage(null);
     onClose();
   };
 
-  const onCrop = (view) => {
-    setImgCrop(view);
-  };
-
-  // Add 'willReadFrequently' attribute to the canvas element
-  const canvasProps = {
-    canvas: {
-      width: 200,
-      height: 200,
-      style: {
-        willReadFrequently: true,
-      },
-    },
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedImage(file);
   };
 
   return (
@@ -69,19 +65,15 @@ const ProfileChange = ({ onProfileSave, onClose, user_id, animal_name, sex, birt
         <label>입양일:</label>
         <input type="text" value={petBirth} onChange={(e) => setPetBirth(e.target.value)} />
       </div>
-      <div className="avatar-container">
-        <Avatar
-          {...canvasProps}
-          width={200}
-          height={200}
-          onCrop={onCrop}
-          onClose={() => setImgCrop(null)}
-        />
+      <div className="form-group">
+        <label>사진:</label>
+        <input type="file" accept="image/*" onChange={handleImageChange} />
       </div>
       <Button onClick={handleUpdateButtonClick} label="추가" />
       <Button onClick={onCloseModal} label="닫기" className="p-button-secondary" />
     </div>
   );
 };
+
 
 export default ProfileChange;
